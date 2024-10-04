9e2b11aaa674f6a693ab698281ff6e8e20898c07
2zm00 <ljm010301@naver.com>
2024-10-04 20:36:39 +0900

    change Navigationbar layout, add info text

--- nawatta/app/components/Navbar.tsx
+++ nawatta/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { useSession, signIn, signOut } from 'next-auth/react';
 import { SessionProvider } from "next-auth/react"
 import { useRouter } from 'next/navigation'
-import { IoMdLogIn, IoMdLogOut } from "react-icons/io";
+import { IoMdLogIn, IoMdLogOut, IoIosList, IoMdInformationCircleOutline } from "react-icons/io";
 
 const NavbarContent = () => {
 const { data: session } = useSession();
@@ -13,19 +13,26 @@ const router = useRouter()
 
   return (
 	<div>
-    <nav className="bg-opacity-30 backdrop-blur-md text-black p-4">
+    <nav className="sticky top-0 bg-opacity-30 backdrop-blur-md text-black p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-xl letter-spacing-3 font-bold flex items-center shadow-lg ">
+        <Link href="/" className="px-4 py-1 rounded-md text-xl letter-spacing-3 font-bold flex items-center hover:bg-slate-200">
 
           ZMO
         </Link>
-        <div className='flex items-center space-x-4'>
-          <Link href="/guestbook" className="flex items-center">
+        <div className='flex items-center space-x-1'>
+          
+          <Link href="/guestbook" className="px-4 py-1 border-solid border border-zinc-400 rounded-md hover:bg-slate-200 flex items-center">
+            <IoIosList className='mr-2'/>
             방명록
           </Link>
+          <Link href="/info" className="px-4 py-1  rounded-md hover:bg-slate-200 flex items-center">
+          <IoMdInformationCircleOutline className="mr-1"/>
+          정보
+          </Link>
+
           {session ? (
             <div className='flex items-center space-x-4'>
-              <span className="mr-4 ">안녕하세요, <a className='shadow-md underline decoration-3 decoration-indigo-500 '>{session.user?.name}</a>님!</span>
+              <span className="mr-4 ml-4 ">안녕하세요, <a className='shadow-md underline decoration-3 decoration-indigo-500 font-bold '>{session.user?.name}</a>님!</span>
               <button onClick={() => signOut()} className="bg-red-500 text-white shadow-lg shadow-red-500/50 hover:bg-red-300 px-3 py-1 rounded flex items-center opacity-80 font-thin ">
               <IoMdLogOut className="mr-2" />
                 로그아웃
--- nawatta/app/info/page.tsx
+++ nawatta/app/info/page.tsx
@@ -1,14 +1,29 @@
 import Link from "next/link";
+import { IoLinkSharp, IoLogoGithub  } from "react-icons/io5";
 
 export default function Info() {
 	return(
 	<div className="flex flex-col items-center justify-center min-h-screen py-2 ">
 		<h1 className="text-4xl mb-8">
-			여기에 정보가 입력될 예정입니다
+			Jeongmo Lee
 		</h1>
+		<h3 className="text-3xl mb-8">
+			I'm i'm zmo
+		</h3>
+		<div className='container mx-auto flex justify-between items-center'>
+			<Link href="https://github.com/2zm00" className=" px-4 py-1 mb-8">
+				<IoLinkSharp className="mr-1"/>
+					Link
+		</Link>
+		</div>
+
+
 		<Link href="/" className="text-xl mb-8 bg-purple-700 text-white rounded-md border-spacing-2 px-4 py-2 border-white   hover:bg-purple-400 hover:shadow-inner shadow-md ">
 			돌아가기
 		</Link>
+		<div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+			
+		</div>
 
 	</div>
 
