2815797992c7f2edaf8d4a2ab4512f8b6b5144e5
2zm00 <ljm010301@naver.com>
2025-03-13 22:21:33 +0900

    fix: Info 링크 버튼 url 변경

--- nawatta/components/info/InfoLayout.tsx
+++ nawatta/components/info/InfoLayout.tsx
@@ -6,12 +6,12 @@ export default function InfoLayout() {
 		<div className="flex flex-col justify-center items-center pt-60">
 			<div className="box-content grid grid-cols-1 text-black mx-auto ">
 				<div className=" text-black pb-16 text-center">
-					Let me Introduce my self
+					저에 대해 소개합니다
 				</div>
 
-				<Link href="https://leezmodev.vercel.app"
+				<Link href="https://jeongmoprofile.vercel.app/"
 				className="flex flex-auto justify-center items-center text-center border-spacing-10 border rounded-full hover:bg-rose-400 hover:shadow-lg shadow-md bg-rose-500 py-32 px-32 ">
-						<h1 className="text-5xl text-white text-opacity-80 "> Here It IS! </h1>
+						<h1 className="text-5xl text-white text-opacity-80 "> 여기요! </h1>
 				</Link>
 
 				<Link href="/" className="text-xl mx-auto mt-32 bg-purple-700 text-white rounded-md border-spacing-2 px-4 py-2 border-white   hover:bg-purple-400 hover:shadow-inner shadow-md ">
